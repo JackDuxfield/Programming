ce64a32818cbebe1553a100061e501de26397e14
Duxfield <jack.duxfield@gmail.com>
2025-02-23 21:09:57 +1300

    Initial

---
+++ Lab0/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+string filePath = "C:\\Users\\Duxfield\\Documents\\Yr2 UCOL\\Programming\\labText.txt";
+string[] lines = File.ReadAllLines(filePath);
+
+int secondNum;
+int loopNum = 0;
+int lineLength = lines.Length;
+var rng = new Random();
+
+while (loopNum < lineLength){
+    loopNum = 0;
+    int firstNum = 0;
+
+    foreach (string line in lines){
+        secondNum = Convert.ToInt32(line);
+
+        if (secondNum < firstNum){
+            Console.WriteLine("[{0}]", string.Join(", ", lines));
+
+            rng.Shuffle(lines);
+            break;
+
+        } else{
+            firstNum = secondNum;
+            loopNum++;
+        }
+    }
+    
+}
+
+Console.WriteLine("\n\n[{0}]", string.Join(", ", lines));
+
+static class RandomExtensions
+{
+public static void Shuffle<T> (this Random rng, T[] array){
+    int n = array.Length;
+    
+    while (n > 1){
+        int k = rng.Next(n--);
+        T temp = array[n];
+        array[n] = array[k];
+        array[k] = temp;
+    }
+}
+}
+
+
---
+++ Lab0FastSort/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        string filePath = "C:\\Users\\Duxfield\\Documents\\Yr2 UCOL\\Programming\\labText.txt";
+        string[] tempLines = File.ReadAllLines(filePath);
+
+        int lineLength = tempLines.Length;
+        int[] lines = new int[lineLength];
+
+        int i = 0;
+        foreach (string tempLine in tempLines)
+        {
+            lines[i] = Convert.ToInt32(tempLine);
+            i++;
+
+        }
+
+        int[] sortedLines = new int[lineLength];
+        int loop = -1;
+
+        foreach (int line in lines)
+        {
+            int sortedLoop = 0;
+            loop++;
+            
+            foreach (int sortedLine in sortedLines)
+            {
+
+                if (sortedLine >= line)
+                {
+                    sortedLines = Sort(sortedLines, sortedLoop, line, loop);
+                    break;
+
+                } else if (sortedLoop == loop)
+                {
+                    sortedLines[sortedLoop] = line;
+                    break;
+
+                } else 
+                {
+                    sortedLoop++;
+
+                }
+            }  
+        }
+
+        Console.WriteLine("\n\n[{0}]", string.Join(", ", sortedLines));
+
+        stopwatch.Stop();
+        TimeSpan elapsed = stopwatch.Elapsed;
+
+        Console.WriteLine($"Time to run: {elapsed.TotalMilliseconds} ms");
+        
+    }
+    public static int[] Sort(int[] array, int index, int newNum, int numSorted)
+        {
+            int tempStore;
+            while (index <= array.Length)
+            {
+                
+                tempStore = array[index];
+                array[index] = newNum;
+                newNum = tempStore;
+
+                if (numSorted == index){
+                    break;
+                } 
+                index++;
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", array));
+            return array;
+        }
+            
+}
