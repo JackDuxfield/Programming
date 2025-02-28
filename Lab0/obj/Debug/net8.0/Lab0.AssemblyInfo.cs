fcc6d55b102bf6c2932ff15c6396513c797f74c4
Duxfield <jack.duxfield@gmail.com>
2025-02-28 13:56:37 +1300

    Lab1 created

--- Lab0/obj/Debug/net8.0/Lab0.AssemblyInfo.cs
+++ Lab0/obj/Debug/net8.0/Lab0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2a474f9ac707fe5b0103f0a1e487ff04088b71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int[] numbers = [1,2,3,4,5];
+
+        PrintArray(numbers);
+        int bigNum = MaxValue(numbers);
+
+        Console.WriteLine("Max Value = " + bigNum);
+    }
+
+    public static void PrintArray(int[] arr | int[,] arrMatrix ){
+        for (int i = 0; i < 5; i++){
+            Console.Write(arr[0,i].ToString());
+        }   
+        Console.WriteLine(); 
+    }
+
+    public static int MaxValue(int[] arr){
+        return arr.Max();
+    }
+
+    public static void CreateMatrix(int[] arr){
+
+        int[,] arrMatrix = new int[5,5];
+        
+        Console.WriteLine("write 5 numbers you wish to store");
+
+        for (int i = 0; i < 5; i++){
+            arrMatrix[0,i] = arr [i];
+
+            while(true){
+                try{
+                    arrMatrix[1,i] = Int32.Parse(Console.ReadLine());
+                    break;
+                }catch (FormatException){
+                    Console.WriteLine("Write a fucking number.");
+                }
+            }
+
+        }
+
+
+
+
+
+         
+    }
+}
