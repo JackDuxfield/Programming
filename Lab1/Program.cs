6449c160b07a87c8364b21245dc1ede60c749cb0
Duxfield <jack.duxfield@gmail.com>
2025-03-09 14:47:03 +1300

    Almost finished lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,34 +1,103 @@
 ﻿using System.Globalization;
+using System.Diagnostics.CodeAnalysis;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        //class methods
+        Person Jack = new Person("Jack", 24, [null]);
+        Person Perry = new Person("Perry", 14, [null]);
+        Person Duxfield = new Person("Duxfield", 39, [null]);
+        Person Sarah = new Person("Sarah-jane", 54, [Jack, Perry, Duxfield]);
+
+        Sarah.DisplayInfo();
+
+        //Array methods
         int[] numbers = [1,2,3,4,5];
 
         PrintArray(numbers);
         int bigNum = MaxValue(numbers);
 
-        Console.WriteLine("Max Value = " + bigNum);
+        Console.WriteLine("\nMax Value = " + bigNum +"\n");
+
+        int[,] arrMatrix = null;
+        bool matrixLoop = true;
+        do{
+            Console.Write("\nWould you like to add to array? y/n: ");
+            string newMatrix = Console.ReadLine();
+            switch (newMatrix){
+                case "y":
+                arrMatrix = CreateMatrix(numbers);
+                matrixLoop = false;
+                break;
+
+                case "n":
+                matrixLoop = false;
+                break;
+
+                default:
+                Console.WriteLine("\nPlease answer y or n\n");
+                break;
+
+        }
+        } while (matrixLoop);
+
+        if (arrMatrix != null){
+            PrintMatrixArray(arrMatrix);
+        }else{
+            PrintArray(numbers);
+        }
+
+        //List methods using numbers array
+        LinkedList newList = new LinkedList();
+        newList.Add(1);
+        newList.Add(2);
+        newList.Add(3);
+        newList.Add(4);
+        newList.Add(5);
+        newList.Add(6);
+        newList.PrintList();
+
+        newList.Delete(4);
+
+        newList.PrintList();
+
     }
 
-    public static void PrintArray(int[] arr | int[,] arrMatrix ){
+
+    //for matrix/array printing
+
+    public static void PrintArray(int[] arr){
         for (int i = 0; i < 5; i++){
-            Console.Write(arr[0,i].ToString());
+            Console.Write($"arr[i].ToString()");
         }   
-        Console.WriteLine(); 
+        Console.WriteLine("\n\n"); 
     }
+
+    public static void PrintMatrixArray(int[,] arr){
+        int i = 0;
+        Console.Write("\n[");
+        do {
+            for (int x = 0; x<5; x++){    
+            Console.Write($"{arr[i,x].ToString()}");
+            if (x<4) Console.Write(", ");
+            }
+            i++;
+        }  while (i<2);
+        Console.Write("]\n\n"); 
+    }
+    
 
     public static int MaxValue(int[] arr){
         return arr.Max();
     }
 
-    public static void CreateMatrix(int[] arr){
+    public static int[,] CreateMatrix(int[] arr){
 
         int[,] arrMatrix = new int[5,5];
         
-        Console.WriteLine("write 5 numbers you wish to store");
+        Console.WriteLine("Write 5 numbers you wish to store");
 
         for (int i = 0; i < 5; i++){
             arrMatrix[0,i] = arr [i];
@@ -38,16 +107,120 @@ internal class Program
                     arrMatrix[1,i] = Int32.Parse(Console.ReadLine());
                     break;
                 }catch (FormatException){
-                    Console.WriteLine("Write a fucking number.");
+                    Console.WriteLine("Write a number.");
                 }
             }
-
         }
+        return arrMatrix;
+    }
+}
+
+public class Node{
+    public int Data;
+    public Node Next;
+
+    public Node(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
 
 
+public class LinkedList{
+    private Node head;
+
+    public void Add(int data){
+        Node newNode = new Node(data);
+        //checks to see if first
+        if (head == null)
+        {
+            head = newNode;
+        } else
+        {
+            //makes current node the first node
+            Node current = head;
+            //checks to see if is the last node, if not sets the current node to the next node.
+            //essentially just a foreach loop to find the last node.
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            //when at last node sets the next node to the new data
+            current.Next = newNode;
+        }
+    }
+
+    public void PrintList(){
+        Node current = head;
+        while( current != null){
+            Console.WriteLine(current.Data + " -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("List End\n\n");
+    }
+
+    public void Delete(int num){
+        Node current = head;
+        Node previous = head;
+        while (current != null){
+
+            //checks if the data = data we are looking for
+            //Changes the previous list link to go to the next link of this chain skipping deleted node
+            //sets deleted node to 0/null
+            if (current.Data == num){
+                previous.Next = current.Next;
+
+                current.Data = 0;
+                current.Next = null;
+                break;
+
+            }
+            previous = current;
+            current = current.Next;
+        }
+    }
+}
 
 
+public class Person {
+    private string name;
+    public string Name
+    {
+        get{ return name;}
+        set{name = value;}
+    }
+    private int age;
+    public int Age
+    {
+        get{ return age;}
+        set{Age = value;}
+    }
+    private Person?[] children;
+    public Person?[] Children
+    {
+        get{ return children;}
+        set{children = value;}
+    }
 
-         
+    public Person(string newName, int newAge, Person?[] newChildren) {
+        name = newName;
+        age = newAge;
+        children = newChildren;
+    }
+
+    public void DisplayInfo(){
+        Console.WriteLine("Name: " + Name + " \nAge: " + Age);
+        if (Children[0] != null){ 
+            Console.WriteLine("Number of children: " + Children.Length);
+            foreach (Person? Child in Children){
+                if (Child != null){
+                    Console.Write("Childs info: ");
+                    Child.DisplayInfo();
+                }
+            }
+            
+        }
     }
 }
